e2f52120dea62466bcf4f463ac958180d23bb418
Josue Rodriguez <zhippuden@gmail.com>
2020-01-26 15:43:49 +0100

    migrated to MongoDB Atlas Cluster.

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export const connect = async () => {
-	const URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/test';
+	const URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/dentalcareclinic';
 	try {
 		await mongoose.connect(URI, {
 			useNewUrlParser: true,
@@ -9,7 +9,7 @@ export const connect = async () => {
 			useFindAndModify: false,
 			useUnifiedTopology: true
 		});
-		console.log(`>>>DB is connected!<<<`);
+		console.log(`>>>Cloud DB is connected!<<<`);
 	} catch (e) {
 		console.log(`¡Something goes wrong!`);
 		console.log(e);
